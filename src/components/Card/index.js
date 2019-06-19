2dbeb2771a6c1c21fce3a190fda7f4514205a069
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-19 09:19:27 +0200

    Add styles

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -10,12 +10,14 @@ const Card = props => {
         <img className="Card__image" src={imageSrc} alt={name} />
         <p className="Card__id">ID / {id}</p>
       </div>
-      <div className="Card__content">
+      <div className="Card__body">
         <h3 className="Card__name">{name}</h3>
         <div className="Card__types">
           {types.map(item => {
             return (
-              <p className="Card__types-type" key={item.type.name} >{item.type.name.toUpperCase()}</p>
+              <p className="Card__types-type" key={item.type.name}>
+                {item.type.name.toUpperCase()}
+              </p>
             );
           })}
         </div>
--- src/services/getListService.js
+++ src/services/getListService.js
@@ -1,6 +1,6 @@
-const URL = 'https://pokeapi.co/api/v2/pokemon/';
+const URL = "https://pokeapi.co/api/v2/pokemon/";
 
 const getList = () => {
-    return fetch(`${URL}?limit=25`).then(res => res.json());
-}
+  return fetch(`${URL}?limit=25`).then(res => res.json());
+};
 export default getList;
