40e3dfc467fe6cd6fc5bac9d105b9f7698d6f4ca
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-19 08:38:47 +0200

    Craete flter by name

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 import Card from "../Card/index";
 
 const List = props => {
-  const { pokemons } = props;
+  const { pokemons, pokemonByName } = props;
   return (
     <ul className="List">
-      {pokemons.map(item => {
+      {pokemons
+      .filter(item => item.name.includes(pokemonByName))
+      .map(item => {
         const { id, name, types } = item;
         return (
           <li key={id}>
