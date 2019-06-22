33e3f8aaf685fe64655df17aa1fcb6b8f3cebd4a
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-22 08:50:18 +0200

    Fix arrow error and change background

--- src/components/PokemonPage/index.js
+++ src/components/PokemonPage/index.js
@@ -3,6 +3,8 @@ import "./styles.scss";
 import PropTypes from "prop-types";
 import getEvolution from "../../services/getEvolutionService";
 import arrow from "../../images/arrow.svg";
+import backArrow from "../../images/back-arrow.svg";
+import { Link } from "react-router-dom";
 
 function changeUnits(x) {
   return x / 10;
@@ -23,10 +25,14 @@ class PokemonPage extends React.Component {
   fetchEvolution() {
     getEvolution(this.props.selectedPokemon.evolutionUrl).then(data => {
       const evolutionPokemons = [data.chain.species.name];
-      evolutionPokemons.push(data.chain.evolves_to[0].species.name);
-      evolutionPokemons.push(
-        data.chain.evolves_to[0].evolves_to[0].species.name
-      );
+      const evo1 = data.chain.evolves_to[0].species.name;
+      const evo2 = data.chain.evolves_to[0].evolves_to[0].species.name;
+      if (data.chain.species && evo1) {
+        evolutionPokemons.push(evo1);
+      }
+      if (data.chain.species && evo2) {
+        evolutionPokemons.push(evo2);
+      }
 
       this.setState({
         evolutionDetails: evolutionPokemons
@@ -37,64 +43,74 @@ class PokemonPage extends React.Component {
   render() {
     const { loading, selectedPokemon } = this.props;
     const { evolutionDetails } = this.state;
+    let pokeAbilities = "";
+    selectedPokemon.abilities.map(item => {
+      return pokeAbilities += `${item.ability.name}, `;
+    });
+
     return (
       <React.Fragment>
         {loading ? (
           <p>Loading...</p>
         ) : (
           <div className="PokemonPage">
-            <div className="PokemenPage__header">
-              <h2 className="PokemonPage__name">{selectedPokemon.name}</h2>
-            </div>
-            <div className="PokemonPage__body">
-              <img
-                className="PokemonPage__image"
-                src={selectedPokemon.sprites.front_default}
-                alt={selectedPokemon.name}
-              />
-
-              <div className="PokemonPage__types">
-                <ul>
-                  {selectedPokemon.types.map(item => {
-                    return (
-                      <li
-                        className="PokemonPage__types-type"
-                        key={item.type.name}
-                      >
-                        {item.type.name.toUpperCase()}
-                      </li>
-                    );
-                  })}
-                </ul>
+            <div className="PokemonPage__wrapper">
+              <div className="PokemenPage__header">
+                <h2 className="PokemonPage__name">{selectedPokemon.name}</h2>
               </div>
-              <p className="PokemonPage__id">#{selectedPokemon.id}</p>
+              <div className="PokemonPage__body">
+                <img
+                  className="PokemonPage__image"
+                  src={selectedPokemon.sprites.front_default}
+                  alt={selectedPokemon.name}
+                />
+
+                <div className="PokemonPage__types">
+                  <ul>
+                    {selectedPokemon.types.map(item => {
+                      return (
+                        <li
+                          className="PokemonPage__types-type"
+                          key={item.type.name}
+                        >
+                          {item.type.name.toUpperCase()}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+                <p className="PokemonPage__id">#{selectedPokemon.id}</p>
+              </div>
+              <div className="PokemonPage__profile-header">
+                <h1 className="Pokemon__profile-title">Detalles</h1>
+              </div>
+              <div className="PokemonPage__profile">
+                <p className="PokemonPage__height">
+                  {`Altura: ${changeUnits(selectedPokemon.height)} m`}
+                </p>
+                <p className="PokemonPage__weight">
+                  {`Peso: ${changeUnits(selectedPokemon.weight)} kg`}
+                </p>
+                <p className="PokemonPage__abilities">
+                  {`Habilidades: ${pokeAbilities.substring(0, pokeAbilities.length - 2)}`}
+                </p>
+              </div>
+              <div className="PokemonPage__evolution-header">
+                <h1 className="PokemonPage__evolution-header-title">
+                  Evolución
+                </h1>
+              </div>
+              <div className="PokemonCard__evolution">
+                <p>{evolutionDetails[0]}</p>
+                <img className="arrow" src={arrow} alt="flecha" />
+                <p>{evolutionDetails[1]}</p>
+                <img className="arrow" src={arrow} alt="flecha" />
+                <p>{evolutionDetails[2]}</p>
+              </div>
             </div>
-            <div className="PokemonPage__profile-header">
-              <h1 className="Pokemon__profile-title">Detalles</h1>
-            </div>
-            <div className="PokemonPage__profile">
-              <p className="PokemonPage__height">
-                {`Altura: ${changeUnits(selectedPokemon.height)} m`}
-              </p>
-              <p className="PokemonPage__weight">
-                {`Peso: ${changeUnits(selectedPokemon.weight)} kg`}
-              </p>
-              <p className="PokemonPage__abilities">
-                {selectedPokemon.abilities.map(item => {
-                  return `${item.ability.name} `;
-                })}
-              </p>
-            </div>
-            <div className="PokemonPage__evolution-header">
-              <h1 className="PokemonPage__evolution-header-title">Evolución</h1>
-            </div>
-            <div className="PokemonCard__evolution">
-              <p>{evolutionDetails[0]}</p>
-              <img className="arrow" src={arrow} alt="flecha" />
-              <p>{evolutionDetails[1]}</p>
-              <img className="arrow" src={arrow} alt="flecha" />
-              <p>{evolutionDetails[2]}</p>
-            </div>
+            <Link to="/">
+              <img className="backArrow" src={backArrow} alt="Volver atrás" />
+            </Link>
           </div>
         )}
       </React.Fragment>
