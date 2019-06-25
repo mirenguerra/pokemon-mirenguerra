373db4ce2ce5559114e79e0e6657a0eb11080ae7
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-25 18:25:22 +0200

    Create new docs

---
+++ docs/precache-manifest.79e977343c2d1330fda2f77a77f9a1a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5ef1fa01df33827b52df46a95dcbbc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe048d68c01228243f26",
+    "url": "./static/css/main.5cbfb543.chunk.css"
+  },
+  {
+    "revision": "af6444347c59973dd0eb",
+    "url": "./static/js/2.b293499a.chunk.js"
+  },
+  {
+    "revision": "fe048d68c01228243f26",
+    "url": "./static/js/main.7a8577ff.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3ea9033931be59445698a31805f5bd8c",
+    "url": "./static/media/arrow.3ea90339.svg"
+  },
+  {
+    "revision": "a078cd7bfdecd04026b38d1804852a3e",
+    "url": "./static/media/back-arrow.a078cd7b.svg"
+  }
+]);
